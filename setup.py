b2aebed6357d34e228f49045762c478a4fbb6f4a
Zack Polizzi <zplizzi@gmail.com>
2016-12-30 23:16:54 -0500

    tagging release w.t.f.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='ipython_import',
+setup(name='ipython_importer',
       version='0.1',
-      description='Allows ipython notebooks to be imported in other notebooks',
+      description='Allows Ipython notebooks to be imported like regular python files',
       url='none',
       author='Zack Polizzi',
       author_email='email',
-      license='MIT',
-      packages=['ipython_import'],
+      license='Apache 2.0',
+      packages=['ipython_importer'],
       zip_safe=False)
